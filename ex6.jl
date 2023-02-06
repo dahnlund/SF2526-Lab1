e1f76ef38a5db1ff8d1fda45ef158b96ea85762d
dahnlund <david.ahnlund@gmail.com>
2023-02-06 10:15:03 +0100

    uppdaterat ex6

--- ex6.jl
+++ ex6.jl
@@ -45,21 +45,26 @@ function QR_greedy(V, p = minimum(size(V)))
     return Q,R,error
 end
 
+# Part A 
+
 Q, R, error = QR_greedy(A)
 tmp = svd(R)
 U_hat = tmp.U
 S = Diagonal(tmp.S)
 V = tmp.V
 U = Q*U_hat
-A_comp = U*S*V'
+plot_6A = plot(error)
+savefig(plot_6A, "./figures/plot_6A")
 
-println("Error between A and SVD from Algo 1:")
-display(norm(A-A_comp))
+# Part C
 
-for i in 1:56
-    ex_vec = A_comp[:,i]
-    image = reshape(ex_vec, size_)
-    image = map(clamp01nan, image)
-    image_name = @sprintf("./compressed_images/comp%04d.png",i)
-    save(image_name, colorview(RGB, image))
+for k = [5 10 20]
+    A_comp = U[:,1:k]*S[1:k,1:k]*V[:,1:k]'
+    for i in 1:56
+        ex_vec = A_comp[:,i]
+        image = reshape(ex_vec, size_)
+        image = map(clamp01nan, image)
+        image_name = @sprintf("./compressed_images_rank%01d/comp%04d.png",k,i)
+        save(image_name, colorview(RGB, image))
+    end
 end
