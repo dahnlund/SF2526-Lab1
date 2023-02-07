ad0672c82108ebe6d0b5274f22d61da34dfbde9f
dahnlund <david.ahnlund@gmail.com>
2023-02-07 20:00:55 +0100

    updated code

--- ex8.jl
+++ ex8.jl
@@ -16,3 +16,25 @@ item5 = get(zalando_items, "item5", 0)
 
 
 zalando_plot(item5[:,1])
+#=
+function [C,Z]=ID_col(A,kk)
+    #=
+    A naive way to compute column ID by via the CPQR-factorization.
+    
+    kk = number of vectors wanted
+    C is selection of the columns of A
+    For sufficiently large kk, we hope that
+      A approx C*Z
+    First: Essentially same as Algorithm 1 (but with pivoting) with kk steps
+    =#
+    [Q,R,P]=qr(A); 
+    Qs=Q(:,1:kk); 
+    Rs=R(1:kk,:);
+    # Now: Compute the ID col
+    R11=Rs(??)
+    R12=Rs(??)
+    C=A*P(:,??);
+    I=eye(kk,kk);
+    Z= [I, inv(R11)*R12]*P';
+end
+#=
